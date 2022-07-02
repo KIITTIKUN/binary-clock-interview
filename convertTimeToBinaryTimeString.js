a8f54a81ce9976174105c922c79472d7f52f5f95
KIITTIKUN <kittikun.real@gmail.com>
2022-07-02 15:35:18 +0700

    feat(getCurrenTime): add getCurrentTime for get current time

--- convertTimeToBinaryTimeString.js
+++ convertTimeToBinaryTimeString.js
@@ -1,5 +1,17 @@
-const convertTimeToBinaryTimeString = (time) => {
+const convertTimeToBinary = (time) => {
   return time
     .split('')
-    .map((time) => parseInt(time, 10).toString(2).padStart(4, '0'));
+    .map((eachTimeUnit) =>
+      parseInt(eachTimeUnit, 10).toString(2).padStart(4, '0')
+    );
 };
+
+const getCurrentTime = () => {
+  const currentTime = new Date();
+  const hours = currentTime.getHours().toString().padStart(2, '0');
+  const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+  const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+  return `${hours}${minutes}${seconds}`;
+};
+
+console.log(convertTimeToBinaryTimeString(getCurrentTime()));
